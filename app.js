d6b2aa1ab4c4ffdf82ea40bdd1a01fee85481d61
dom-m17 <dominicmaynard00@gmail.com>
2024-07-23 13:42:08 +0100

    adding post activities

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const app = express();
 
 app.use(helmet());
 
+app.use(express.json());
+
 app.get( '/', (req, res) => {
     res.send('Hello world!')
 })
@@ -20,4 +22,27 @@ app.get( '/activities', (req, res) => {
     res.json(activities)
 })
 
+app.post('/activities', (req, res) => {
+    let activityType = req.body.activity_type;
+    let activityDuration = req.body.activity_duration;
+    let uniqueIdentifier = randomIdGenerator()
+    let timestamp = Date.now()
+    activities.push({
+        uniqueIdentifier,
+        timestamp,
+        activityType,
+        activityDuration
+    })
+    res.json(activities[activities.length - 1])
+})
 
+
+function randomIdGenerator() {
+    let randomId = Math.floor((Math.random() * 1000000))
+    for (let i=0; i<activities.length; i++) {
+        if (randomId === activities[i].id) {
+            randomIdGenerator()
+        } 
+    }
+    return randomId
+}
