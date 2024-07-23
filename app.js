d042b3dcd8efb447d620927b56ac750ca6a45b74
dom-m17 <dominicmaynard00@gmail.com>
2024-07-23 11:51:30 +0100

    adding helmet middleware

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import helmet from 'helmet';
 
 const port = 3000;
 const app = express();
+
+app.use(helmet());
 
 app.get( '/', (req, res) => {
     res.send('Hello world!')
